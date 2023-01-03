9efb1f3c338f38d3d0a15c96d25ffa001dda6913
Davidson Mizael <davidsonmizael@gmail.com>
2023-01-03 04:11:36 -0300

    Adding missing columns

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS cerebro.asn_data (id SERIAL PRIMARY KEY, country_code TEXT, ip_range_start TEXT, ip_range_end TEXT, create_date timestamp, last_seen_date timestamp, last_scan_date timestamp);
-CREATE TABLE IF NOT EXISTS cerebro.ip (id SERIAL PRIMARY KEY, asn_data_id INT, ipv4 TEXT, ipv6 TEXT, hostname TEXT, status TEXT);
+CREATE TABLE IF NOT EXISTS cerebro.ip (id SERIAL PRIMARY KEY, asn_data_id INT, ipv4 TEXT, ipv6 TEXT, hostname TEXT, state TEXT, create_date timestamp, last_update timestamp);
 CREATE TABLE IF NOT EXISTS cerebro.task (name TEXT PRIMARY KEY, frequency TEXT, last_run_date timestamp, status TEXT);
 CREATE TABLE IF NOT EXISTS cerebro.task_run (id SERIAL PRIMARY KEY, task_name TEXT, start_date timestamp, end_date timestamp, status TEXT, additional_info TEXT);
 CREATE TABLE IF NOT EXISTS cerebro.task_config (id SERIAL PRIMARY KEY, task_name TEXT, config_name TEXT, config_value TEXT);
